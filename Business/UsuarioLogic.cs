14ab37b8f038a922cb5c8c6590f9f17928c77ae9
FelipeMirande <felipemirande@gmail.com>
2018-06-20 17:33:41 -0300

    magia 2

---
+++ Business/UsuarioLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessEntities;
+
+
+namespace BusinessLogic
+{
+    public class UsuarioLogic : BusinessLogic
+    {
+        public UsuarioLogic()
+        {
+            UsuarioData = new Data.Database.UsuarioAdapter();
+        }
+
+
+        private Data.Database.UsuarioAdapter _UsuarioData;
+        public Data.Database.UsuarioAdapter UsuarioData
+        {
+            set { _UsuarioData = value; }
+            get { return _UsuarioData; }
+        }
+
+        public BusinessEntities.Usuario GetOne(int ID)
+        {
+            return UsuarioData.GetOne(ID);
+        }
+        public List<Usuario> GetAll()
+        {
+            return UsuarioData.GetAll();
+        }
+
+        public void Save(BusinessEntities.Usuario usuario)
+        {
+            UsuarioData.Save(usuario);
+        }
+
+        public void Delete(int ID)
+        {
+            UsuarioData.Delete(ID);
+        }
+    }
+}
