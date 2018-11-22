20c64fc86305025421fe31205b99ac0fddc9d74f
lisandroferroni <lisandro.ferroni@gmail.com>
2018-11-22 06:00:59 -0300

    profesor avanzado

---
+++ UI.Web/ProfesorPages/CursosProfesor.aspx
@@ -0,0 +1,108 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="CursosProfesor.aspx.cs" Inherits="UI.Web.CursosProfesor" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="Contenido" runat="server">
+    <center><asp:Panel ID="gridPanel" runat="server" Height="161px">
+        <asp:GridView ID="gridCursos" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" AutoGenerateColumns="False" DataKeyNames="ID" OnSelectedIndexChanged="gridCursos_SelectedIndexChanged" OnRowDataBound="gridCursos_RowDataBound">
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:BoundField DataField="ID" HeaderText="ID" />
+                <asp:BoundField DataField="IDCurso" HeaderText="Materia" />
+                <asp:BoundField DataField="IDCurso" HeaderText="Comision" />
+                <asp:BoundField DataField="IDCurso" HeaderText="Año" />
+                <asp:CommandField SelectText="Seleccionar" ShowSelectButton="True" >
+                <ControlStyle Font-Bold="True" />
+                </asp:CommandField>
+            </Columns>
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+        <asp:Button ID="btnVer" runat="server" OnClick="btnVer_Click" Text="Ver" />
+        <asp:GridView ID="GridAlumnos" runat="server" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="ID" ForeColor="#333333" GridLines="None" OnRowDataBound="GridAlumnos_RowDataBound" OnSelectedIndexChanged="gridAlumnos_SelectedIndexChanged">
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:BoundField DataField="ID" HeaderText="ID" />
+                <asp:BoundField DataField="IDAlumno" HeaderText="Legajo" />
+                <asp:BoundField DataField="IDAlumno" HeaderText="Nombre" />
+                <asp:BoundField DataField="IDAlumno" HeaderText="Apellido" />
+                <asp:BoundField DataField="Nota" HeaderText="Nota" />
+                <asp:BoundField DataField="Condicion" HeaderText="Condicion" />
+                <asp:CommandField SelectText="Seleccionar" ShowSelectButton="True">
+                <ControlStyle Font-Bold="True" />
+                </asp:CommandField>
+            </Columns>
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+            <asp:Button ID="btnEditar" runat="server" OnClick="btnEditar_Click" Text="Editar" Visible="False" />
+            <asp:Button ID="btnSalir" runat="server" OnClick="btnSalir_Click" Text="Salir" Visible="False" />
+
+        <asp:Panel ID="Panel2" runat="server" Height="289px">
+            
+            <br />
+            <br />
+            <br />
+            <asp:Panel ID="formPanel" runat="server" Height="256px" style="margin-top: 0px" Visible="False">
+                <table dir="ltr">
+                    <tr>
+                        <td dir="rtl">Nombre</td>
+                        <td>
+                            <asp:TextBox ID="txtNombre" runat="server" ReadOnly="True" Enabled="False"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td dir="rtl" style="height: 26px">Apellido</td>
+                        <td style="height: 26px">
+                            <asp:TextBox ID="txtApellido" runat="server" Enabled="False"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td dir="rtl">Nota</td>
+                        <td>
+                            <asp:TextBox ID="txtNota" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td dir="rtl">Condición</td>
+                        <td>
+                            <asp:DropDownList ID="ddlCondicion" runat="server">
+                            </asp:DropDownList>
+                        </td>
+                    </tr>
+                </table>
+                <table dir="ltr">
+                    <tr>
+                        <td dir="rtl">
+                            <asp:Button ID="btnAceptar" runat="server" OnClick="btnAceptar_Click" Text="Aceptar" />
+                        </td>
+                        <td>
+                            <asp:Button ID="btnCancelar" runat="server" OnClick="btnCancelar_Click" Text="Cancelar" />
+                        </td>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="color: #FF0000">
+                            <asp:Label ID="lblError" runat="server"></asp:Label>
+                        </td>
+                    </tr>
+                </table>
+            </asp:Panel>
+        </asp:Panel>
+    </asp:Panel></center>
+</asp:Content>
---
+++ UI.Web/ProfesorPages/CursosProfesor.aspx.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLogic;
+using BusinessEntities;
+
+namespace UI.Web
+{
+    public partial class CursosProfesor : System.Web.UI.Page
+    {
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            if (Session["tipo"].ToString() != "Profesor")
+            {
+                Response.Redirect("http://localhost:57900/Home.aspx");
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadGrid();
+                //ddlcondicion?
+            }
+        }
+        
+        public enum FormModes
+        {
+            Alta,
+            Baja,
+            Modificacion
+        }
+
+        public FormModes FormMode
+        {
+            get { return (FormModes)ViewState["FormMode"]; }
+            set { ViewState["FormMode"] = value; }
+        }
+
+        protected int SelectedID
+        {
+            get
+            {
+                if (ViewState["SelectedID"] != null)
+                {
+                    return (int)ViewState["SelectedID"];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                ViewState["SelectedID"] = value;
+            }
+        }
+
+        protected int SelectedIDAlumno
+        {
+            get
+            {
+                if (ViewState["SelectedIDAlumno"] != null)
+                {
+                    return (int)ViewState["SelectedIDAlumno"];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                ViewState["SelectedIDAlumno"] = value;
+            }
+        }
+
+
+        private Curso _CursoActual;
+        public Curso CursoActual
+        {
+            get { return _CursoActual; }
+            set { _CursoActual = value; }
+        }
+
+        private Materia _MateriaActual;
+        public Materia MateriaActual
+        {
+            get { return _MateriaActual; }
+            set { _MateriaActual = value; }
+        }
+
+        private Comision _ComisionActual;
+        public Comision ComisionActual
+        {
+            get { return _ComisionActual; }
+            set { _ComisionActual = value; }
+        }
+
+        private Usuario _UsuarioActual;
+        public Usuario UsuarioActual
+        {
+            get { return _UsuarioActual; }
+            set { _UsuarioActual = value; }
+        }
+
+        private Persona _PersonaActual;
+        public Persona PersonaActual
+        {
+            get { return _PersonaActual; }
+            set { _PersonaActual = value; }
+        }
+
+        private AlumnoInscripcion _AlumnoInscripcion;
+        public AlumnoInscripcion AlumnoInscripcionActual
+        {
+            get { return _AlumnoInscripcion; }
+            set { _AlumnoInscripcion = value; }
+        }
+
+        protected bool IsEntitySelected
+        {
+            get
+            {
+                return SelectedID != 0;
+            }
+        }
+    
+
+        private void LoadGrid()
+            {
+                DocenteCursoLogic dcl = new DocenteCursoLogic();
+                gridCursos.DataSource = dcl.GetAll((int)Session["id"]);
+                gridCursos.DataBind();
+
+
+            }
+
+        private void LoadGridAlumnos()
+                {
+                    InscripcionLogic il = new InscripcionLogic();
+                    GridAlumnos.DataSource = il.GetAllByCurso(SelectedID);
+                    GridAlumnos.DataBind();
+                }
+
+        protected void btnVer_Click(object sender, EventArgs e)
+        {
+            if (IsEntitySelected)
+            {
+                gridCursos.Visible = false;
+                btnVer.Visible = false;
+                GridAlumnos.Visible= true;
+                btnEditar.Visible = true;
+                btnSalir.Visible = true;
+                LoadGridAlumnos();
+            }
+        }
+
+
+
+        //CursoLogic _logic;
+        //private CursoLogic Logic
+        //{
+        //    get
+        //    {
+        //        if (_logic == null)
+        //        {
+        //            _logic = new CursoLogic();
+        //        }
+        //        return _logic;
+        //    }
+        //}
+
+        //private Curso _Entity;
+        //public Curso Entity
+        //{
+        //    set { _Entity = value; }
+        //    get { return _Entity; }
+        //}
+
+        private void LoadForm(int id)
+        {
+            PersonaLogic pl = new PersonaLogic();
+            UsuarioLogic ul = new UsuarioLogic();
+            InscripcionLogic il = new InscripcionLogic();
+            UsuarioActual = ul.GetOne(id);
+            PersonaActual = pl.GetOne(UsuarioActual.IDPersona);
+            AlumnoInscripcionActual = il.GetOne(id);
+
+            txtApellido.Text = PersonaActual.Apellido;
+            txtNombre.Text = PersonaActual.Nombre;
+            txtNota.Text = AlumnoInscripcionActual.Nota.ToString();
+            
+        }
+
+        private void LoadEntity()
+        {
+            switch (FormMode)
+            {
+                
+                case FormModes.Modificacion:
+                    AlumnoInscripcionActual = new AlumnoInscripcion();
+                    AlumnoInscripcionActual.Nota = int.Parse(txtNota.Text);
+                    AlumnoInscripcionActual.Condicion = (AlumnoInscripcion.TiposCondiciones)int.Parse(ddlCondicion.SelectedValue);
+                    AlumnoInscripcionActual.ID = SelectedIDAlumno;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SaveEntity(AlumnoInscripcion alumnoInscripcion )
+        {
+            InscripcionLogic il = new InscripcionLogic();
+            il.Save(alumnoInscripcion);
+        }
+
+
+        //private void DeleteEntity(int id)
+        //{
+        //    Logic.Delete(id);
+        //}
+
+        //private void ClearForm()
+        //{
+        //    txtID.Text = string.Empty;
+        //    txtMateria.Text = string.Empty;
+        //    txtAño.Text = string.Empty;
+        //    txtCupo.Text = string.Empty;
+        //}   
+
+
+
+        //protected void btnEliminar_Click(object sender, EventArgs e)
+        //{
+        //    if (IsEntitySelected)
+        //    {
+        //        DeleteEntity(SelectedID);
+        //        LoadGrid();
+        //    }
+        //}
+
+        protected void btnAceptar_Click(object sender, EventArgs e)
+        {
+            //if (Validar())
+            //{
+                LoadEntity();
+                SaveEntity(AlumnoInscripcionActual);
+                LoadGridAlumnos();
+                formPanel.Visible = false;
+            //}
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LoadGridAlumnos();
+            formPanel.Visible = false;
+        }
+
+        protected void gridCursos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedID = (int)gridCursos.SelectedValue;
+        }
+
+        protected void gridAlumnos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            InscripcionLogic il = new InscripcionLogic();
+            int aux = (int)GridAlumnos.SelectedValue;
+            AlumnoInscripcionActual = il.GetOne(aux);
+
+            SelectedIDAlumno = AlumnoInscripcionActual.IDAlumno; 
+        }
+
+        protected void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (IsEntitySelected)
+            {
+                formPanel.Visible = true;
+                FormMode = FormModes.Modificacion;                
+                LoadForm(SelectedIDAlumno);
+
+            }
+        }
+
+        protected void btnSalir_Click(object sender, EventArgs e)
+        {
+            gridCursos.Visible = true;
+            btnVer.Visible = true;
+            GridAlumnos.Visible = false;
+            btnEditar.Visible = false;
+            btnSalir.Visible = false;
+            formPanel.Visible = false;
+            LoadGrid();
+        }
+
+     
+        
+
+        protected void gridCursos_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            CursoLogic cl = new CursoLogic();
+
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                if (e.Row.Cells[1].Text != null)
+                {
+                    int id = int.Parse(e.Row.Cells[1].Text);
+                    CursoActual = cl.GetOne(id);
+                    MateriaLogic ml = new MateriaLogic();
+                    MateriaActual = ml.GetOne(CursoActual.IDMateria);
+                    e.Row.Cells[1].Text = MateriaActual.Descripcion;
+                }
+
+                if (e.Row.Cells[2].Text != null)
+                {
+                    int id = int.Parse(e.Row.Cells[2].Text);
+                    CursoActual = cl.GetOne(id);
+                    ComisionLogic cml = new ComisionLogic();
+                    ComisionActual = cml.GetOne(CursoActual.IDComision);
+                    e.Row.Cells[2].Text = ComisionActual.Descripcion;
+                }
+
+                if (e.Row.Cells[3].Text != null)
+                {
+                    int id = int.Parse(e.Row.Cells[3].Text);
+                    e.Row.Cells[3].Text = CursoActual.AnioCalendario.ToString();
+                }
+            }
+        }
+
+        protected void GridAlumnos_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            UsuarioLogic ul = new UsuarioLogic();
+            PersonaLogic pl = new PersonaLogic();
+            
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                if (e.Row.Cells[1].Text != null)
+                {
+                    int id = int.Parse(e.Row.Cells[1].Text);
+                    UsuarioActual = ul.GetOne(id);
+                    PersonaActual = pl.GetOne(UsuarioActual.IDPersona);
+                    e.Row.Cells[1].Text = PersonaActual.Legajo.ToString();
+                    e.Row.Cells[2].Text = PersonaActual.Nombre.ToString();
+                    e.Row.Cells[3].Text = PersonaActual.Apellido.ToString();
+                }
+                
+            }
+
+
+        }
+
+        
+
+        //public bool Validar()
+        //{
+        //    if (string.IsNullOrEmpty(txtAño.Text) || string.IsNullOrEmpty(txtMateria.Text) || cbxComision.SelectedValue == null || string.IsNullOrEmpty(txtCupo.Text))
+        //    {
+        //        lblError.Visible = true;
+        //        lblError.Text = "Debe llenar todos los campos";
+        //        return false;
+        //    }
+
+        //    if (txtAño.Text.Length != 4)
+        //    {
+        //        lblError.Visible = true;
+        //        lblError.Text = "Ingrese correctamente el año";
+        //        return false;
+        //    }
+
+        //    if (Logic.EstaAgregado(int.Parse(txtMateria.Text), int.Parse(cbxComision.SelectedValue), int.Parse(txtAño.Text)))
+        //    {
+        //        lblError.Visible = true;
+        //        lblError.Text = "Ya existe ese curso en esa comision";
+        //        return false;
+        //    }
+
+        //    return true;
+        //}
+    }
+}
